27e3f2af27cd7ee58966d9f0ae2cc5a1f7d5e164
isaacs <i@izs.me>
2023-10-05 00:12:07 -0700

    missing snapshot

---
+++ tap-snapshots/test/default-colors.ts.test.cjs
@@ -0,0 +1,13 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/default-colors.ts > TAP > must match snapshot 1`] = `
+Object {
+  "DEFAULT_BG": "#222222",
+  "DEFAULT_FG": "#eeeeee",
+}
+`
